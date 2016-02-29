dbd3bc0f1286557b59de800179768193e1b8ca52
Programmer Rammer <rtkq.kvalifikacija@gmail.com>
2016-02-29 17:17:57 +0200

    Top navbar layout changes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'contact'             => 'static_pages#contact'
 
   resources :products, only: [:show, :index]
+  resources :categories, only: [:show, :index]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
