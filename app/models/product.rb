4430b8e3ba14c1124d6aacae10260bf98be750a1
Programmer Rammer <rtkq.kvalifikacija@gmail.com>
2016-03-10 14:18:44 +0200

    Product index layout

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,8 @@ class Product < ActiveRecord::Base
 	belongs_to :department, dependent: :destroy
 
 	has_attached_file :image,
-										:styles  => { :thumb => "200x200>" },
+										:styles  => { :thumb => "x200" },
+										:convert_options => {:'x200' => " -background white -gravity center -extent x200"},
 	    							:storage => :dropbox,
 								    :dropbox_credentials => { app_key: ENV['APP_KEY'],
 								                              app_secret: ENV['APP_SECRET'],
