b12c449a06525020dff8c3d7389616590fc53698
Programmer Rammer <rtkq.kvalifikacija@gmail.com>
2016-03-31 17:10:06 +0300

    Ajax cart functionality and qjuery animation

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,21 +18,3 @@ class ProductsController < ApplicationController
 	private
   
 end
-
-
-
-
-
-
-#def show
-#  	@user = User.find(params[:id])
-#    @microposts = @user.microposts.paginate(page: params[:page])
-#  end
-#
-#  def new
-#  	@user = User.new
-#  end
-#
-#  def index
-#    @users = User.paginate(page: params[:page])
-#  end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,8 @@
 class StaticPagesController < ApplicationController
-  
+
+  include CurrentCart
+  before_action :set_cart
+
   def home
   end
 
