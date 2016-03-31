bca382a95fbb2331db005c69b27a1bf3898b67ff
Programmer Rammer <rtkq.kvalifikacija@gmail.com>
2016-03-31 20:32:56 +0300

    Improvments to seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
 
 
 print "\n 1 - Seed manual categories and department data from seed file \n 2 - Seed generated data \n"
@@ -13,6 +7,7 @@ choice = STDIN.gets.chomp
 
 
 if choice == 1 
+
   #dach = department and category hash
   #Each key is department and key value is array of categories
   dach = {'Computers'           => ['Cases', 'Cooling', 'Memory (RAM)',
@@ -36,24 +31,37 @@ if choice == 1
                                      'Microphones', 'Cases', 'Styluses',
                                      'Storage', 'Cooling', 'Chargers',
                                      'Webcams', 'Carrying bags', 'Storage',
-                                     'Wrist rests']
+                                     'Wrist rests'],
+
+          'Laptops'             =>  ['Accessories', 'Bags', 'Batteries',
+                                     'Link']
   }
 
-
-  dach.each do |key, array|
-    puts "#{key}-----"
-    puts array
-  end
-
-
   #Generate departments
-  5.times do
+  puts "== CREATING manual departments =="
+  c = 0
     dach.each do |key, array|
+      c += 1
       description = Faker::Lorem.sentence
-      Department.create!(name: "#{key}",
-                           description: description)
+      Department.create!(name: "#{key}")
+      puts "#{c}: #{key}"
     end
+    puts "== DONE #{dach.count} departments ==\n\n"
+
+  #Generate categories for each department
+  puts "== CREATING manual categories =="
+  c = 0
+  dach.each do |key, array|
+      puts "---#{key}---"
+    array.each do |name|
+      c += 1
+      Category.create!(name: "#{name}")
+      puts "#{c}: #{name}"
+    end
+    puts "\n"
   end
+
+  puts "== DONE #{c} categories ==\n\n"
 
 
   #if choice == 1
@@ -61,73 +69,88 @@ if choice == 1
 
 
 
-  else
-    #Seed database with these variables:
-    #Categories are generated for each department between 1..5
-    department_count = 5
-    product_count = 10
-    user_count = 10
+else
+  #Seed database with these variables:
+  #Categories are generated for each department between 1..5
+  department_count = 5
+  product_count = 10
+  user_count = 10
+
+  #Generate departments
+  c = 0
+  puts "== CREATING departments =="
+  department_count.times do
+    c += 1
+  	name = Faker::Commerce.department
+  	description = Faker::Lorem.sentence
+  	Department.create!(name:  name)
+    puts "#{c}: #{name}"
+  end
+  puts "== DONE, created #{department_count} departments ==\n\n"
+
+
+  #Generate random count of categories on each deapartment
+  puts "== CREATING categories =="
+  d = 0
+  c = 0
+  department_count.times do 
+    d += 1
+    x = Faker::Number.between(1, 5)
+    x.times do
+      c += 1
+      name = Faker::Commerce.department
+      department_id = d
+      Category.create!(name:  name,
+                      department_id: department_id)
+      puts "#{c}: #{name}, department id = #{department_id}"
+    end
+  end
+  puts "== DONE, 1..5 categories for each of #{department_count} departments ==\n\n"
+end
+
+
+#List of image file names on dropbox, so they are randomly assigned
+image_array = ["iphone.jpg", "pc.jpeg", "keyboard.jpeg", "laptop.jpg", "smartphone.jpg"]
+
+#Generate products
+c = 0
+puts "== CREATING products =="
+category_count = Category.count
+product_count.times do
+  c += 1
+  title  = Faker::Commerce.product_name
+  description = Faker::Lorem.sentence
+  price = Faker::Commerce.price
+  department_id = Faker::Number.between(1, 5)
+  category_id = Faker::Number.between(1, category_count)
+  Product.create!(title:  title,
+                  description: description,
+                  price: price,
+                  department_id: department_id,
+                  category_id: category_id,
+                  image_file_name: image_array.sample)
+  puts "#{c}: #{title}, price = #{price}"
+end
+puts "== CREATED #{product_count} products ==\n\n"
+
+#Generate users
+c = 0
+puts "== CREATING users =="
+user_count.times do
+    c += 1
+    email = Faker::Internet.email
+    password = Faker::Internet::password(8, 20)
+    User.create!(email: email,
+                 password: password)
+ puts "#{c}: #{email}, password = #{password}"
+end
+puts "== DONE, created #{user_count} users\n\n" 
 
     #Generate test user
-    puts "== CREATED #{user_count} users =="
-    User.create!(email: "admin@example.com",
-                 password: "12345678")
-    puts "== User created, see in seeds file =="
+User.create!(email: "admin@example.com",
+             password: "12345678")
+puts "== User created, see in seeds file =="
 
 
-    #Generate departments
-    department_count.times do
-    	name = Faker::Commerce.department
-    	description = Faker::Lorem.sentence
-    	Department.create!(name:  name,
-      								description: description)
-    end
-    puts "== CREATED #{department_count} departments =="
 
-
-    #Generate random count of categories on each deapartment
-    d = 0
-    department_count.times do 
-      d += 1
-      x = Faker::Number.between(1, 5)
-      x.times do
-        name = Faker::Commerce.department
-        department_id = d
-        Category.create!(name:  name,
-                        department_id: department_id)
-      end
-    end
-    puts "== CREATED random count of categories for #{department_count} departments =="
-
-
-    #List of image file names on dropbox, so they are randomly assigned
-    image_array = ["iphone.jpg", "pc.jpeg", "keyboard.jpeg", "laptop.jpg", "smartphone.jpg"]
-
-    #Generate products
-    category_count = Category.count
-    product_count.times do
-      title  = Faker::Commerce.product_name
-      description = Faker::Lorem.sentence
-      price = Faker::Commerce.price
-      department_id = Faker::Number.between(1, 5)
-      category_id = Faker::Number.between(1, category_count)
-      Product.create!(title:  title,
-                      description: description,
-                      price: price,
-                      department_id: department_id,
-                      category_id: category_id,
-                      image_file_name: image_array.sample)
-    end
-    puts "== CREATED #{product_count} products =="
-
-    #Generate users
-
-    user_count.times do
-        email = Faker::Internet.email
-        password = Faker::Internet::password(8, 20)
-        User.create!(email: email,
-                     password: password)
-    end
-
-    puts "== DONE =="
-end
+puts "== DATABASE SEEDING DONE =="
