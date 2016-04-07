de4612d7b7de47233800537ad9c6acda3e1b48ae
Programmer Rammer <marisancans@gmail.com>
2016-04-07 20:12:48 +0300

    Category partial fix, titles depending on category

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,6 +10,7 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.where("category_id = ?", params[:selected]).paginate(page: params[:page])
+    @current_category = Category.find(params[:selected]).name
   end
  # @products = Product.where("category_id = ?", params[:selected])
   #  .map{|p| [p.title, p.price] }#.paginate(page: params[:page]) Get only title, price etc. 
