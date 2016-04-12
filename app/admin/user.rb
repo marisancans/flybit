63e36aae3f0517fff3fe63b392bda5b3b1245af5
Programmer Rammer <marisancans@gmail.com>
2016-04-12 16:01:48 +0300

    AA users, cart removed from show

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -16,6 +16,19 @@ ActiveAdmin.register User do
     actions dropdown: true 
   end
 
+  show do
+    attributes_table do
+      row :email
+      row :sign_in_count
+      row :current_sign_in_at
+      row :last_sign_in_at
+      row :current_sign_in_ip
+      row :last_sign_in_ip
+      row :created_at
+      row :updated_at
+    end
+  end
+
   form do |f|
   	f.semantic_errors
   		inputs 'Details' do
