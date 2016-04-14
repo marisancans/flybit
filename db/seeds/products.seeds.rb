fea583d1b929d02503815a804dfeac0d92ff7077
Programmer Rammer <marisancans@gmail.com>
2016-04-14 18:03:37 +0300

    Show cart image preview fix

--- db/seeds/products.seeds.rb
+++ db/seeds/products.seeds.rb
@@ -1,3 +1,4 @@
+image_array = ["iphone.jpg", "keyboard.jpeg", "vlad.jpg", "laptop.jpg", "smartphone.jpg", "earbuds.jpeg", "mouse.jpg", "router.jpg"]
 print "How many products? : "
 choice = STDIN.gets.chomp.to_i
 category_count = Category.count
@@ -9,13 +10,14 @@ c += 1
   price = Faker::Commerce.price
   department_id = Faker::Number.between(1, 5)
   category_id = Faker::Number.between(1, category_count)
+  img = image_array.sample
   Product.create!(title:  title,
                   description: description,
                   price: price,
                   department_id: department_id,
                   category_id: category_id)
+  puts "#{c}: #{title}, price = #{price}, image = #{img}"
   Attachment.create(image: Rails.root.join("public/uploads/seeds/#{img}").open, 
-                    product_id: Product.last.id))
-  puts "#{c}: #{title}, price = #{price}"
+                    product_id: Product.last.id)
 end
 puts '-------CREATED #{choice} products-------'
