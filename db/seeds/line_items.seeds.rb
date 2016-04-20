5a3ecb6c1df8b0e147ca2e351926608ab0e3d8c6
Maris <marisancans@gmail.com>
2016-04-20 19:08:14 +0300

    Cart summary

--- db/seeds/line_items.seeds.rb
+++ db/seeds/line_items.seeds.rb
@@ -1,11 +1,12 @@
-cart_id = Cart.last
+cart_id = Cart.last.id
 puts "Last cart = #{cart_id}"
 puts "p_id | c_id | q"
 5.times do
 	product_id = Faker::Number.between(1, 10)
+	quantity = Faker::Number.between(1, 10)
 	LineItem.create!(product_id: product_id,
 									 cart_id: cart_id,
-									 quantity: 10)
+									 quantity: quantity)
 	puts "#{product_id} | #{cart_id} | #{quantity}"
 
 end
