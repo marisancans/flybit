c7e1ecb401d11aa55bddf8560c6861dddaa9e589
Maris <marisancans@gmail.com>
2016-04-25 10:42:28 +0300

    Orders seed file generates line items for display

--- db/seeds.rb
+++ db/seeds.rb
@@ -209,8 +209,9 @@ puts "== DONE, created #{user_count} users ==\n\n"
 #Generate orders
 c = 0
 puts "== CREATING orders =="
+product_count = Product.count
 User.last(10).reverse.each do |user|
-c += 1
+  c += 1
   Order.create!(name: Faker::Name.name , 
                 address: Faker::Address.street_address, 
                 email: user.email, 
@@ -220,9 +221,13 @@ c += 1
                 city: Faker::Address.city,
                 country: Faker::Address.country,
                 country_code: Faker::Address.country_code)
+  Faker::Number.between(1, 10).times do
+    LineItem.create!(product_id: Faker::Number.between(1, product_count),
+                     quantity: Faker::Number.between(1, 10),
+                     order_id: Order.last.id)
+  end
   puts "#{c}: email = #{user.email}, user id: #{user.id}"
 end
-puts "== DONE, created #{user_count} users ==\n\n" 
 
 #Generate test user
 if User.exists?(email: "user@example.com")
--- db/seeds/orders.seeds.rb
+++ db/seeds/orders.seeds.rb
@@ -1,8 +1,9 @@
 #Generate orders
 c = 0
 puts "== CREATING orders =="
+product_count = Product.count
 User.last(10).reverse.each do |user|
-c += 1
+  c += 1
   Order.create!(name: Faker::Name.name , 
                 address: Faker::Address.street_address, 
                 email: user.email, 
@@ -12,5 +13,10 @@ c += 1
                 city: Faker::Address.city,
                 country: Faker::Address.country,
                 country_code: Faker::Address.country_code)
+  Faker::Number.between(1, 10).times do
+    LineItem.create!(product_id: Faker::Number.between(1, product_count),
+                     quantity: Faker::Number.between(1, 10),
+                     order_id: Order.last.id)
+  end
   puts "#{c}: email = #{user.email}, user id: #{user.id}"
 end
