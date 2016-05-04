c5f3bfb14b435c36e3a82112fb418db9e2e97acb
Maris <marisancans@gmail.com>
2016-05-04 11:40:18 +0300

    Cart show discount ribbon on product image, price fixes

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -5,7 +5,11 @@ class LineItem < ActiveRecord::Base
 	belongs_to :cart, :counter_cache => true
 
  	def total_price
+ 		if !product.discount.blank?
+  		(product.price - product.discount) * quantity
+  	else
   		product.price * quantity
+  	end
   end
 
   def check_qty
