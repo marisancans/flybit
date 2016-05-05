06d1f5df7667f3f32f42089147b13874c0440c50
Maris <marisancans@gmail.com>
2016-05-05 14:23:44 +0300

    Devis login custom form

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -5,6 +5,8 @@ Devise.setup do |config|
   config.secret_key = ENV["devise_secret_key"] if Rails.env.production?
   config.http_authenticatable_on_xhr = false
   config.navigational_formats = ["*/*", :html, :json]
+
+  config.scoped_views = true
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
