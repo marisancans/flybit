767d64f88dfd830cba5c960bd138ad174af0a198
Maris <marisancans@gmail.com>
2016-05-06 12:33:56 +0300

    category seeds and original seeds file updated to upload images

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,31 +14,66 @@ if choice == 1
 
   #dach = department and category hash
   #Each key is department and key value is array of categories
-  dach = {'Computers'           => ['Cases', 'Cooling', 'Memory (RAM)',
-                                    'Motherboards','Power supplies', 'Processors', 
-                                    'Video cards','Sound cards', 'Storage',
-                                    'Pre-buillt PCs', 'All-in-one PCs', 'Monitors'],
+  dach = {'Computers'           => [['Cases', 'computers_cases.png'], 
+                                    ['Cooling', 'computers_cooling.png'], 
+                                    ['Memory (RAM)', 'computers_memory.png'],
+                                    ['Motherboards', 'computers_motherboard.png'],
+                                    ['Power supplies', 'computers_power_supply.png'],
+                                    ['Processors', 'computers_processors.png'],
+                                    ['Video cards','computers_video_cards.png'],
+                                    ['Sound cards', 'computers_sound_cards.png'],
+                                    ['Storage', 'computers_storage.png'],
+                                    ['Pre-built PCs', 'computers_pre_built.png'],
+                                    ['All-in-one PCs', 'computers_all_in_one.png']],
 
-          'Tablets'             =>  ['Apple', 'Samsung', 'Microsoft',
-                                     'Lenovo', 'Asus', 'Google', 'Dell', 
-                                     'Acer', '7 inches', '8 inches', 
-                                     '9 inches', '10 inches', '12 inches'],
+        'Tablets'               =>  [['Apple', 'apple.png'],
+                                    ['Samsung', 'samsung.png'],
+                                    ['Microsoft', 'microsoft.png'],
+                                    ['Lenovo', 'lenovo.png'],
+                                    ['Asus', 'asus.png'],
+                                    ['Google', 'google.png'],
+                                    ['Dell', 'dell.png'],
+                                    ['Acer', 'acer.png'],
+                                    ['7 inches', 'tablets_7_inches.jpg'],
+                                    ['8 inches', 'tablets_8_inches.jpg'],
+                                    ['9 inches', 'tablets_9_inches.png'],
+                                    ['10 inches', 'tablets_10_inches.jpg'],
+                                    ['12 inches','tablets_12_inches.png']],
 
-          'Laptops & Notebooks' =>  ['Apple', 'Dell', 'HP',
-                                     'Lenovo', 'Asus', 'Acer',
-                                     'Alienware', 'Sony', 'Toshiba',
-                                     'Windows', 'Mac OS', 'Chrome OS',
-                                     'No OS', 'Less than $500', '$500 - $1000',
-                                     '$1000+'],
+        'Laptops & Notebooks' =>    [['Apple', 'apple.png'],
+                                    ['Dell',  'dell.png'],
+                                    ['HP', 'hp.png'],
+                                    ['Lenovo', 'lenovo.png'],
+                                    ['Asus', 'asus.png'],
+                                    ['Acer', 'acer.png'],
+                                    ['Alienware', 'alienware.png'],
+                                    ['Sony', 'sony.png'],
+                                    ['Toshiba', 'toshiba.png'],
+                                    ['Windows', 'windows.png'],
+                                    ['Mac OS', 'mac_os.png'],
+                                    ['Chrome OS', 'chrome_os.jpg'],
+                                    ['No OS', 'no_os.png'],
+                                    ['Less than $500', '500_EUR.png'],
+                                    ['$500 - $1000', '500_1000_EUR.png'],
+                                    ['$1000+', '1000_EUR.png']],
 
-          'Accessories'         =>  ['Mice', 'Keyboards', 'Headphones',
-                                     'Microphones', 'Cases', 'Styluses',
-                                     'Storage', 'Cooling', 'Chargers',
-                                     'Webcams', 'Carrying bags', 'Storage',
-                                     'Wrist rests'],
+        'Accessories'         =>    [['Mice', 'mouse.png'],
+                                    ['Keyboards', 'keyboard.png'],
+                                    ['Headphones','headphones.png'],
+                                    ['Microphones', 'microphone.jpg'],
+                                    ['Cases', 'computers_cases.png'],
+                                    ['Styluses', 'styluses.png'],
+                                    ['Storage', 'storage.jpeg'],
+                                    ['Cooling', 'computers_cooling.png'],
+                                    ['Chargers', 'chargers.png'],
+                                    ['Webcams', 'webcams.jpeg'],
+                                    ['Carrying bags', 'carrying_bags.png'],
+                                    ['Wrist rests', 'wrist_rest.png']],
 
-          'Laptops'             =>  ['Accessories', 'Bags', 'Batteries',
-                                     'Link']
+        'Laptops'               =>  [['Accessories', 'laptop_accessories.jpeg'],
+                                    ['Bags', 'computers_cases.png'],
+                                    ['Batteries', 'batteries.png'],
+                                    ['chargers', 'chargers.png']]
   }
 
   #Generate departments
@@ -56,9 +91,12 @@ if choice == 1
   dach.each do |key, array|
       department_id += 1
       puts "---#{key}---"
-      array.each_with_index do |name, category_index|
-      Category.create!(name: "#{name}", department_id: "#{department_id}")
-      puts "#{category_index + 1}|#{department_id}|#{name}"
+      array.each_with_index do |array_of_category, category_index|
+      img = array_of_category[1]
+      Category.create!(name: "#{array_of_category[0]}", 
+                      department_id: "#{department_id}",
+                      image: File.open(File.join(Rails.root,"public/seeds/category_images/#{img}")))
+      puts "#{category_index + 1}|#{department_id}|#{img}"
       category_count += 1
     end
     puts "\n"
