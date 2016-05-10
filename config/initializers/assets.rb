65220c05d273a640d56def9aee2ce56559a086d5
Maris <marisancans@gmail.com>
2016-05-10 09:01:29 +0300

    Profile page panels, links, cols, css fix

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -19,7 +19,7 @@ Rails.application.config.assets.precompile += %w( active_admin.scss departments.
 																									static_pages.css.scss navbar.scss
 																									scaffolds.css carts.scss
 																									charges.css home_page.css.scss
-																									registrations.css.scss searches.css
+																									devise/registrations.css.scss searches.css
 																									navbar-fixed-side.css.scss product_show_page.css.scss
 																									jquery.chocolat.js chocolat.css.scss
 																									carts.css.scss charges.css.scss
