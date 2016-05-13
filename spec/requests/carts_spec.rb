1f2ee05624f7170f773dd9cadef6b0ba1c3f07fa
Maris <marisancans@gmail.com>
2016-05-13 12:00:22 +0300

    spec config, test

--- spec/requests/carts_spec.rb
+++ spec/requests/carts_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Carts", type: :request do
-	user = User.last
+	user = FactoryGirl.create(:user)
+
+ 	it "should add line item " do 
+    visit root_path
+  end
 
 end
